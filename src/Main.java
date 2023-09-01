6dfe7c3aa12e90fce7f39050daeb75c154f50627
Hubert Kraj <frodokraj22@gmail.com>
2023-09-01 17:15:16 +0200

    SubtitlesImporter class created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main
@@ -9,50 +8,17 @@ public class Main
     {
         //Load file
         String subtitlesPath = userInput();
-        Scanner lineScanner = new Scanner(new File(subtitlesPath));
-        ArrayList<Line> lines = new ArrayList<>();
-
-        //Scan file
-        while (lineScanner.hasNextLine())
-        {
-            String section = lineScanner.nextLine();
-            //Skip indentation
-            if (section == null)
-                continue;
-
-            //Prepare line number
-            int lineNumber = Integer.parseInt(section);
-
-            //Prepare start and stop times
-            section = lineScanner.nextLine();
-            String[] times = section.split(" ");
-            times[0] = times[0].replace(",", ".");
-            times[2] = times[2].replace(",", ".");
-            String startTime = times[0];
-            String stopTime = times[2];
-
-            //Prepare text
-            String text = lineScanner.nextLine();
-            ArrayList<String> textList = new ArrayList<>();
-            while (!Objects.equals(text, ""))
-            {
-                textList.add(text);
-                if(!lineScanner.hasNextLine())
-                    break;
-                text = lineScanner.nextLine();
-            }
-            Line newLine = new Line(lineNumber, startTime, stopTime, textList);
-            lines.add(newLine);
-        }
+        SubtitlesImporter subtitlesImporter = new SubtitlesImporter(subtitlesPath);
+        ArrayList<Line> subtitles = subtitlesImporter.importSubtitles();
 
         //Change time
         long change = Long.parseLong(userInput());
-        lines.forEach((n) -> n.startTime = n.startTime.plusNanos( change * 1000000L) );
-        lines.forEach((n) -> n.stopTime = n.stopTime.plusNanos( change * 1000000L) );
+        subtitles.forEach((n) -> n.startTime = n.startTime.plusNanos( change * 1000000L) );
+        subtitles.forEach((n) -> n.stopTime = n.stopTime.plusNanos( change * 1000000L) );
 
         //Create new file
         PrintWriter printWriter = new PrintWriter(new FileWriter(subtitlesPath));
-        lines.forEach((n) ->
+        subtitles.forEach((n) ->
         {
                 printWriter.println(n.number);
                 printWriter.println(n.startTime + " --> " + n.stopTime);
